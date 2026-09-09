8c39155cdbbba1a363e709788cc15aa111077ef9
Felipe6459 <felipemedeiros6459@gmail.com>
2026-09-08 23:56:17 -0300

    Cria aplicativo WebView do GestorPro

---
+++ android/app/src/main/java/br/com/gestorpro/MainActivity.java
@@ -0,0 +1,41 @@
+package br.com.gestorpro;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String HOME = "https://felipe6459.github.io/GestorPro-/";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setAllowFileAccess(false);
+        webView.getSettings().setAllowContentAccess(false);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        webView.loadUrl(HOME);
+    }
+
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) { webView.goBack(); return true; }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override protected void onDestroy() { if (webView != null) webView.destroy(); super.onDestroy(); }
+}
